c0e537c72d827eaa2856d2c8f6439719918e2dfb
Gurleen Phagura <gphagura@github.com>
2016-09-26 12:39:01 -0700

    Added more

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -21,12 +21,42 @@ TEST(FieldTest, placeMineInBounds)
 	
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
-	ASSERT_EQ(MINE_HIDDEN, minefield.get(4,5));
+	ASSERT_EQ(EMPTY_HIDDEN, minefield.get(5,5));
+
 }
-/*TEST(FieldTest, placeMineInBounds)
+TEST(FieldTest, customField)
+{
+	Field minefield = Field(4);
+	minefield.placeMine(4,4);
+	ASSERT_EQ(MINE_HIDDEN ,minefield.get(4,4));
+}
+TEST(FieldTest, isSafe)
 {
 	Field minefield;
+	
 	minefield.placeMine(4,5);
-	ASSERT_EQ("Out of Bounds" ,minefield.get(1,11));
+	ASSERT_EQ(1, minefield.get(4,5) );
+	ASSERT_EQ(0, minefield.get(1,1));
+
 }
-*/
+TEST(FieldTest, isSafeOutOFBOUNDS)
+{
+	Field minefield;
+	
+	minefield.placeMine(4,5);
+	ASSERT_EQ(1, minefield.get(1,11));
+
+}
+TEST(FieldTest, revealAdjacent)
+{
+	Field minefield;
+	
+	minefield.placeMine(5,6);
+	minefield.placeMine(4,5);	
+	minefield.revealAdjacent(5,5);	
+	ASSERT_EQ( MINE_SHOWN, minefield.get(5,6) );
+	ASSERT_EQ(EMPTY_SHOWN, minefield.get(5,4));
+	ASSERT_EQ(MINE_SHOWN, minefield.get(4,5));
+	ASSERT_EQ(EMPTY_SHOWN, minefield.get(6,5));
+
+}
--- buglog.md
+++ buglog.md
@@ -1,2 +1,4 @@
 # BUG LOG
-Didn't get much done as I had to reinstall gtest
+revealAdjacent fails, in the else if statement it doesn't check to see if the col and row are in bounds 
+isSafe fails to show if a area is safe but works if there is a mine
+issafe fails to tell us if the location is out of bounds
